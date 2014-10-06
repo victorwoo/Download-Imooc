600ad7a097e5f49bfb945abc54abde9cd988cdd6
wub <victorwoo@gmail.com>
2014-10-06 21:02:16 +0800

    test.

--- test.cmd
+++ test.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
-powershell -NoProfile -ExecutionPolicy Unrestricted .\Download-Imooc.ps1 75, 197, 156, 203 -Combine -RemoveOriginal
+powershell -NoProfile -ExecutionPolicy Unrestricted .\Download-Imooc.ps1 171, 75, 197, 156, 203 -Combine -RemoveOriginal
 PAUSE
