5a1fa1d0207b7f0880273a1c93ba8109eadb18b3
wub <victorwoo@gmail.com>
2014-10-19 18:29:53 +0800

    Download #156 individually

--- test.cmd
+++ test.cmd
@@ -1,3 +1,4 @@
 @ECHO OFF
-powershell -NoProfile -ExecutionPolicy Unrestricted .\Download-Imooc.ps1 171, 75, 197, 156, 203, 9, 207 -Combine -RemoveOriginal
+powershell -NoProfile -ExecutionPolicy Unrestricted .\Download-Imooc.ps1 171, 75, 197, 203, 9, 207 -Combine -RemoveOriginal
+powershell -NoProfile -ExecutionPolicy Unrestricted .\Download-Imooc.ps1 156 -Combine
 PAUSE
